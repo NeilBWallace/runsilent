697c2721347c5929859f0eceb0bc4d8e36cbf821
Neil Wallace <neilbwallace@gmail>
2017-10-13 18:38:05 +0100

    'mlab'

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 module.exports = {
- // database: 'mongodb://runsilent:runsilent@ds117965.mlab.com:17965/runsilent',
-  database: 'mongodb://localhost:27017/meanauth',    //dev
+  database: 'mongodb://runsilent:runsilent@ds117965.mlab.com:17965/runsilent',
+ // database: 'mongodb://localhost:27017/meanauth',    //dev
   secret: 'yoursecret'
 }
